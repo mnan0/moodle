cc0e2d3cf5d4d97bce97584a785e807855999807
mnan0 <50372901+mnan0@users.noreply.github.com>
2021-08-08 16:17:55 -0400

    change required version

--- htdocs/local/staffmanager/version.php
+++ htdocs/local/staffmanager/version.php
@@ -3,7 +3,7 @@
 // plugin version
 $plugin->version = 2021072801;
 // plugin requires this moodle version (found in version.php in source folder)
-$plugin->requires = 2021072800;
+$plugin->requires = 2021051701;
 // plugin name (must be same as container folder, if not local plugin, use plugin type)
 $plugin->component = 'local_staffmanager';
 //$plugin->cron = 0;
