960997f2de85dddcd5f5c0e58f19ac5138b38c77
mnan0 <50372901+mnan0@users.noreply.github.com>
2021-08-09 13:05:54 -0400

    new searchbar template

--- htdocs/local/staffmanager/index.php
+++ htdocs/local/staffmanager/index.php
@@ -9,7 +9,7 @@ require_once '../../config.php'; // two folders above
 
 global $USER, $DB, $CFG; // useful global variables
 
-$PAGE->set_url('local/staffmanager/index.php');
+$PAGE->set_url('/local/staffmanager/index.php');
 $PAGE->set_context(context_system::instance());
 
 require_login(); // require certain permissions before viewing this page
@@ -18,6 +18,13 @@ $strpagetitle = get_string('staffmanager','local_staffmanager'); //relates to la
 // for page title --> look for local_staffmanager file and then find the string that is called "staffmanager"
 //  then set the title to that
 
+$strpageheading = get_string('staffmanager','local_staffmanager');
+
 $PAGE->set_title($strpagetitle);
+$PAGE->set_heading($strpageheading);
 echo $OUTPUT->header();
+
+// include the template between the header and footer (notice the filepath style, don't need extension)
+// notice the data array is empty, can be filled and sent to the template to be used
+echo $OUTPUT->render_from_template("local_staffmanager/searchbar",[]);
 echo $OUTPUT->footer();
